7a755a881e9ebde3f2e957f8bf46c30ae5a5f6eb
ansty <kirillovaap@yandex.ru>
2021-04-10 20:58:58 +0300

    script

--- DUF.sh
+++ DUF.sh
@@ -1,3 +1,8 @@
 pip install tensorflow==1.3.0
 pip install scipy Pillow h5py
-python3 test_my.py 4 16 L /dataset/test1_bicubic_noise2 /output/DUF
+python3 test_my.py --R 4 --L 16 --T L --test_dir /dataset --video_name test1_bicubic_noise2 --save_dir /output/DUF
+python3 test_my.py --R 4 --L 16 --T L --test_dir /dataset --video_name test1_gauss_noise2 --save_dir /output/DUF
+python3 test_my.py --R 4 --L 16 --T L --test_dir /dataset --video_name test2_bicubic_noise2 --save_dir /output/DUF
+python3 test_my.py --R 4 --L 16 --T L --test_dir /dataset --video_name test2_gauss_noise2 --save_dir /output/DUF
+python3 test_my.py --R 4 --L 16 --T L --test_dir /dataset --video_name test3_bicubic_noise2 --save_dir /output/DUF
+python3 test_my.py --R 4 --L 16 --T L --test_dir /dataset --video_name test3_gauss_noise2 --save_dir /output/DUF
