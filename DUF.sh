df0484e52734ee9816f93d844956d337dd030291
ansty <kirillovaap@yandex.ru>
2021-04-10 23:39:27 +0300

    script

--- DUF.sh
+++ DUF.sh
@@ -1,8 +1,13 @@
 pip install tensorflow==1.3.0
 pip install scipy Pillow h5py
-python3 test_my.py --R 4 --L 16 --T L --test_dir /dataset --video_name test1_bicubic_noise2 --save_dir /output/DUF
-python3 test_my.py --R 4 --L 16 --T L --test_dir /dataset --video_name test1_gauss_noise2 --save_dir /output/DUF
+python3 test_my.py --R 4 --L 28 --T L --test_dir /dataset --video_name test1_bicubic_noise2 --save_dir /output/DUF
+
 python3 test_my.py --R 4 --L 16 --T L --test_dir /dataset --video_name test2_bicubic_noise2 --save_dir /output/DUF
 python3 test_my.py --R 4 --L 16 --T L --test_dir /dataset --video_name test2_gauss_noise2 --save_dir /output/DUF
 python3 test_my.py --R 4 --L 16 --T L --test_dir /dataset --video_name test3_bicubic_noise2 --save_dir /output/DUF
 python3 test_my.py --R 4 --L 16 --T L --test_dir /dataset --video_name test3_gauss_noise2 --save_dir /output/DUF
+
+python3 test_my.py --R 4 --L 28 --T L --test_dir /dataset --video_name test1_gauss_noise2 --save_dir /output/DUF
+python3 test_my.py --R 4 --L 28 --T L --test_dir /dataset --video_name test2_bicubic_noise2 --save_dir /output/DUF
+python3 test_my.py --R 4 --L 28 --T L --test_dir /dataset --video_name test2_gauss_noise2 --save_dir /output/DUF
+
